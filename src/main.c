9651a4873be25e9ac44ee42be44168ef5a5acdc6
Ciro García Belmonte <ciro.garcia.belmonte@gmail.com>
2022-12-10 18:07:39 +0100

    Attempts to make scaling better

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cigarcia <cigarcia@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 04:32:33 by cigarcia          #+#    #+#             */
-/*   Updated: 2022/12/10 17:24:46 by cigarcia         ###   ########.fr       */
+/*   Updated: 2022/12/10 18:04:38 by cigarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,15 @@ void	loop(void *param)
 int	main(void)
 {
 	t_data		*data;
-	mlx_image_t	*i;
 
 	data = init_data();
 	data->wall_north = mlx_load_png("assets/north_test.png");
-	i = mlx_new_image(data->mlx, 200, 200);
-	for (int x = 0; x < (int)i->width; x++)
-		for (int y = 0; y < (int)i->height; y++)
-			mlx_put_pixel(i, x, y, x * y << 8 | 0xFF);
-	mlx_image_to_window(data->mlx, i, 10, 10);
-	mlx_image_to_window(data->mlx, scale_image(data->mlx, i, (t_vector){0.5,
-				0.5}), 210, 210);
+	draw_texture_area_scaled(data->mlx, data->img, data->wall_north, (t_vector[4]){
+			(t_vector){0, 0},
+			(t_vector){32, 32},
+			(t_vector){2, 1},
+			(t_vector){0, 0},
+	});
 	mlx_loop_hook(data->mlx, loop, data);
 	mlx_key_hook(data->mlx, key_hook, data);
 	mlx_loop(data->mlx);
--- src/render/texture_utils.c
+++ src/render/texture_utils.c
@@ -6,7 +6,7 @@
 /*   By: cigarcia <cigarcia@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 15:08:33 by cigarcia          #+#    #+#             */
-/*   Updated: 2022/12/10 17:25:40 by cigarcia         ###   ########.fr       */
+/*   Updated: 2022/12/10 18:03:37 by cigarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,14 +29,13 @@ mlx_image_t	*cropped_texture(mlx_t *mlx, mlx_texture_t *texture,
 
 uint32_t	mlx_get_pixel(mlx_image_t *image, int x, int y)
 {
-	uint8_t		*pixel;
+	uint32_t	*pixel;
 	uint32_t	color;
 
-	if (x < 0 || y < 0 || x > image->width || y > image->height)
+	if (x < 0 || y < 0 || x > (int)image->width || y > (int)image->height)
 		return (-1);
-	pixel = &image->pixels[(int)(y * image->height + x) * 4];
-	color = (*(pixel) << 24 | *(pixel + 1) << 16 | *(pixel + 2) << 8 | *(pixel
-				+ 3));
+	pixel = (uint32_t *)&image->pixels[(int)(y * image->height + x) * 4];
+	color = revert_bits(*pixel, 32);
 	return (color);
 }
 
@@ -53,11 +52,34 @@ mlx_image_t	*scale_image(mlx_t *mlx, mlx_image_t *img, t_vector scale)
 		x = 0;
 		while (x < new->width)
 		{
-			mlx_put_pixel(new, x, y, mlx_get_pixel(img, x / scale.x, y
+			mlx_put_pixel(new, x, y, mlx_get_pixel(img, ((double)x) / scale.x, ((double)y)
 						/ scale.y));
 			x++;
 		}
 		y++;
 	}
 	return (new);
+}
+
+void	draw_texture_area_scaled(mlx_t *mlx, mlx_image_t *img, mlx_texture_t *texture, t_vector *area)
+{
+	mlx_image_t *temp;
+	mlx_image_t *temp2;
+	int x;
+	int y = 0;
+
+	temp = cropped_texture(mlx, texture, area[0], area[1]);
+	temp2 = scale_image(mlx, temp, area[2]);
+	mlx_delete_image(mlx,temp);
+	while (y < (int)temp2->height)
+	{
+		x = 0;
+		while (x < (int)temp2->width)
+		{
+			mlx_put_pixel(img, area[3].x + x, area[3].y + y, mlx_get_pixel(temp2, x, y));
+			x++;
+		}
+		y++;
+	}
+	mlx_delete_image(mlx,temp2);
 }
